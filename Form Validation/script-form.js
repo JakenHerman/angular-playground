02f54d21cb83551a130382706e633112ef67ffd3
Jaken Herman <jch053@shsu.edu>
2016-06-07 15:44:35 -0500

    Update script-form.js

--- Form Validation/script-form.js
+++ Form Validation/script-form.js
@@ -1,14 +1,18 @@
 var app = angular.module('formExample', []);
 
-app.directive('wjValidationError', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctl) {
+app.controller('setRequired', ['$scope', function($scope) {
+    $scope.required = true;
+}]);
 
-      scope.$watchGroup(['wjValidationError', 'passwordStrength'], function(errorMsg, value, scope) {
-        elm[0].setCustomValidity(errorMsg);
-        ctl.$setValidity('wjValidationError', errorMsg ? false : true);
-    });
-    }
-  };
-  });
+app.directive('wjValidationError', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctl) {
+
+            scope.$watchGroup(['wjValidationError', 'passwordStrength'], function(errorMsg, value, scope) {
+                elm[0].setCustomValidity(errorMsg);
+                ctl.$setValidity('wjValidationError', errorMsg ? false : true);
+            });
+        }
+    };
+});
